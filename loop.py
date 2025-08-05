7555da461bd82d9781ec141bfbe94ad13c9a4131
Abishan <abishanjeyarajah@gmail.com>
2025-08-05 16:40:19 +0530

    Add loop examples in Python
    Added loop.py with examples of while loops and a placeholder for nestedLoop.py. Demonstrates basic loop constructs and control flow.

---
+++ loop.py
@@ -0,0 +1,25 @@
+x = 0
+'''
+is_bool = True;
+
+
+while is_bool:
+    print(x)
+    x += 1
+    if x > 5:
+        is_bool = False;
+#print(f'x = {x}')
+'''
+
+while x < 10:
+    print("Abishan")
+    x+=1
+    
+is_bool = True;
+
+while is_bool:
+    print("ABishan")
+    x += 1
+    if x > 10:
+        is_bool = False;
+#print(f'x = {x}')
