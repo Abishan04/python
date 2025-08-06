fc6b857c013f7237bdce2af3fc8c93f77d29c0f0
Abishan <abishanjeyarajah@gmail.com>
2025-08-06 09:11:30 +0530

    Remove nested while loop example from loop_1.py
    Deleted a code block demonstrating nested while loops that printed sequences of numbers. This cleans up the file by removing unused or redundant code.

--- loop_1.py
+++ loop_1.py
@@ -97,11 +97,3 @@ while j <= 5:
          print(j,end = '')
     j+=1
       '''
-i = 1
-while i <= 5:
-    j = 1
-    while j <= i:
-        print(j, end = ',' if j < i else '')
-        j += 1
-    print()
-    i += 1
